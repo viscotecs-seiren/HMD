05583874d8301ddc34b1c35659e14b84dd10c9c2
T.Kawabata <kawabata92@outlook.com>
2020-11-03 10:31:43 +0900

    Release Test

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -5,6 +5,8 @@ self.addEventListener("fetch", (event) => {
 		caches.open(CACHE_NAME).then(async (cache) => {
 			const response = await cache.match(event.request);
 
+			if(response){console.log("Hit");}
+
 			return response || fetch(event.request).then((fetch_response) => {
 				cache.put(event.request, fetch_response.clone());
 				return fetch_response;
