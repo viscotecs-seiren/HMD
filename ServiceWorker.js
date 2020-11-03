83449b0932137ec79e44938e41d9a70aadafc79b
T.Kawabata <kawabata92@outlook.com>
2020-11-03 10:27:04 +0900

    Release Test

---
+++ ServiceWorker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'HMD_CACHE';
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		caches.open(CACHE_NAME).then(async (cache) => {
+			const response = await cache.match(event.request);
+
+			return response || fetch(event.request).then((fetch_response) => {
+				cache.put(event.request, fetch_response.clone());
+				return fetch_response;
+			});
+		})
+	);
+})
