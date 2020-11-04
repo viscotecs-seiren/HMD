2631fdfd7a8ae167822b6a48a171b2f3e375ca54
T.Kawabata <kawabata92@outlook.com>
2020-11-04 10:52:43 +0900

    Release Test

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,5 +1,9 @@
 const CACHE_NAME = 'HMD_CACHE';
 
+self.addEventListener('install', (event) => {
+	event.waitUntil(self.skipWaiting());
+ });
+
 self.addEventListener('activate', (event) => {
 	event.waitUntil(self.clients.claim());
 });
