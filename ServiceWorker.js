d1311edb67dfe951f45e1be4cf7edbb21de831eb
T.Kawabata <kawabata92@outlook.com>
2020-11-09 12:46:00 +0900

    Release Test

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -37,19 +37,21 @@ self.addEventListener("fetch", (event) => {
 });
 
 self.addEventListener("message", (event) => {
-	if(event.data["command"] && event.data["command"] === "clearCache") {
-		event.waitUntil(
-			event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
-				cache.keys().then(function(keys) {
-					keys.forEach(function(request) {
-						if(request.url) {
-							if(!request.url.endsWith("_0.jpg")) {
-								cache.delete(request);
+	if(event.data["command"]) {
+		if(event.data["command"] === "clearCache") {
+			event.waitUntil(
+				event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+					cache.keys().then(function(keys) {
+						keys.forEach(function(request) {
+							if(request.url) {
+								if(!request.url.endsWith("_0.jpg")) {
+									cache.delete(request);
+								}
 							}
-						}
+						});
 					});
-				});
-			}))
-		);
+				}))
+			);
+		}
 	}
 });
